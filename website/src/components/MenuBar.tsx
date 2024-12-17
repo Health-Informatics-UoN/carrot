8e6b1436f7a26ac867e1631923095dd3de3ffe33
andrewthien <anretrithien@gmail.com>
2024-12-17 16:49:43 +0000

    test

--- website/src/components/MenuBar.tsx
+++ website/src/components/MenuBar.tsx
@@ -16,7 +16,7 @@ export const MenuBar = () => {
               src="/carrot-logo.png"
               alt="carrot-logo"
             />
-            Carrot test
+            Carrot
           </div>
         </Link>
 
