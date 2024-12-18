af980ad68ce60da85f6c8c202af8e3f12fc25d05
andrewthien <anretrithien@gmail.com>
2024-12-18 14:24:39 +0000

    test

--- website/src/components/homepage/CTA.tsx
+++ website/src/components/homepage/CTA.tsx
@@ -27,7 +27,7 @@ export default function CallToAction() {
             {deployLink != "" && (
               <a href={deployLink} target="_blank">
                 <Button className="lg:text-lg dark:hover:bg-gray-100 hover:bg-gray-100 text-md hover:border-orange-900 dark:hover:border-orange-900 text-orange-600 dark:text-orange-600 bg-white dark:bg-white border border-orange-600">
-                  <Rocket className="mr-2 size-4" /> Deploy your own Carrot
+                  <Rocket className="mr-2 size-4" /> Deploy your own Carrot1
                 </Button>
               </a>
             )}
