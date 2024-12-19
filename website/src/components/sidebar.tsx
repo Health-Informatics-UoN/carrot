b43a319532eb9745641eb611c8b85f14ffc63f4a
andrewthien <anretrithien@gmail.com>
2024-12-19 11:49:08 +0000

    cleaning sideBar

--- website/src/components/sidebar.tsx
+++ website/src/components/sidebar.tsx
@@ -2,12 +2,10 @@
 
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { LogOut, Menu, MoreHorizontal, Settings } from "lucide-react";
+import { Menu } from "lucide-react";
 import Link from "next/link";
 import { SidebarButton } from "./sidebar-button";
 import { usePathname } from "next/navigation";
-import { Separator } from "./ui/separator";
-import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
 import { useEffect, useState } from "react";
 import { ModeToggle } from "./mode-toggle";
 import { sidebarItems } from "./MenuItems";
@@ -50,64 +48,18 @@ export function Sidebar({ userName }: { userName?: string }) {
               </SheetHeader>
               <div className="h-full">
                 <div className="flex mt-7 flex-col w-full gap-1">
-                  {(userName ? sidebarItems.links : sidebarItems.routes).map(
-                    (link, idx) => (
-                      <Link key={idx} href={link.href}>
-                        <SidebarButton
-                          variant={
-                            pathname === link.href ? "secondary" : "ghost"
-                          }
-                          icon={link.icon}
-                          className="w-full"
-                        >
-                          {link.label}
-                        </SidebarButton>
-                      </Link>
-                    )
-                  )}
+                  {sidebarItems.routes.map((link, idx) => (
+                    <Link key={idx} href={link.href}>
+                      <SidebarButton
+                        variant={pathname === link.href ? "secondary" : "ghost"}
+                        icon={link.icon}
+                        className="w-full"
+                      >
+                        {link.label}
+                      </SidebarButton>
+                    </Link>
+                  ))}
                 </div>
-                {userName && (
-                  <div className="absolute w-full bottom-4 px-1 left-0">
-                    <Separator className="absolute -top-3 left-0 w-full" />
-                    <Drawer>
-                      <DrawerTrigger asChild>
-                        <Button
-                          variant="outline"
-                          className="w-full justify-start"
-                        >
-                          <div className="flex justify-between items-center w-full">
-                            <div className="flex gap-2">
-                              <span>{userName}</span>
-                            </div>
-                            <MoreHorizontal size={20} />
-                          </div>
-                        </Button>
-                      </DrawerTrigger>
-                      <DrawerContent className="mb-2 p-2 w-[350px]">
-                        <div className="flex flex-col space-y-2 mt-2">
-                          <a href="/accounts/password_change/">
-                            <SidebarButton
-                              size="sm"
-                              icon={Settings}
-                              className="w-full"
-                            >
-                              Change Password
-                            </SidebarButton>
-                          </a>
-                          <a href={"/accounts/logout/"}>
-                            <SidebarButton
-                              size="sm"
-                              icon={LogOut}
-                              className="w-full"
-                            >
-                              Log Out
-                            </SidebarButton>
-                          </a>
-                        </div>
-                      </DrawerContent>
-                    </Drawer>
-                  </div>
-                )}
               </div>
             </SheetContent>
           </Sheet>
