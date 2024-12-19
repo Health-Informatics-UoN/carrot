e5b490349cac7de232b8c9808f45d55edc63d419
andrewthien <anretrithien@gmail.com>
2024-12-19 14:10:40 +0000

    add projects and upload SR page

--- website/src/pages/mapper/user_guide/_meta.js
+++ website/src/pages/mapper/user_guide/_meta.js
@@ -1,5 +1,5 @@
 export default {
-  quickstart: "Quick Start",
+  quickstart: "Quickstart",
   projects_datasets_scanreports:
     "Projects, Datasets, Scan Reports, and Permissions",
   upload_scan_report: "Uploading a Scan Report file",
