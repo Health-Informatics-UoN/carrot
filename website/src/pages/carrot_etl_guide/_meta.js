8d54c9b2e42787aad0b7903f6348750f2ff59850
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-05 13:29:24 +0100

    Add supported tables documentation

--- website/src/pages/carrot_etl_guide/_meta.js
+++ website/src/pages/carrot_etl_guide/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  supported_tables: ["Supported Tables"],
   pre_processing: "Pre-Processing",
   metadata_profiling: "Metadata Profiling",
   metadata_mapping: "Metadata Mapping",
