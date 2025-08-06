16df705d1699ee41f67d7b6807e1b524f157b7ac
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-06 13:40:59 +0100

    Remove Azure functions documentation (#78)
    * Update standards documentation for CSV file requirements
    
    * Add supported tables documentation
    
    * Add example link
    
    * Update dev guide
    
    * Update configuration documentation for environment variables and deployment requirements
    
    * Remove functions
    
    * Update mea
    
    * Update steps
    
    * Add security model and guidelines documentation
    
    * Update Azure Infrastructure Diagram SVG
    
    * Update website/src/pages/mapper/dev_guide/quickstart.mdx
    
    Co-authored-by: Tri Thien Nguyen <119530400+AndrewThien@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tri Thien Nguyen <119530400+AndrewThien@users.noreply.github.com>

--- website/src/pages/mapper/dev_guide/_meta.js
+++ website/src/pages/mapper/dev_guide/_meta.js
@@ -2,9 +2,4 @@ export default {
   quickstart: "Quickstart",
   data_structures: "Internal data structures",
   mapping_rules: "Mapping rules",
-  // api: "API",
-  // azure_functions: "Azure functions",
-  // migrations: "Database migration",
-  // new_vocab: "Adding new vocabulary",
-  // testing: "Testing",
 };
