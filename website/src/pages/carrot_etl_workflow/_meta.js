dcfc41eb2ad95d8ebc9aa2cdfd9add2772593512
Brian <kiirubrian21@gmail.com>
2025-11-19 13:48:40 +0000

    Move Pre Processing to ETL workflow docs

--- website/src/pages/carrot_etl_workflow/_meta.js
+++ website/src/pages/carrot_etl_workflow/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  pre_processing: "Pre-Processing",
   phase1_whiterabbit: "Phase 1: Data Profiling with WhiteRabbit",
   phase2_mapper: "Phase 2: Data Transformation with Carrot Mapper",
   phase3_execution: "Phase 3: Data Execution and Validation",
