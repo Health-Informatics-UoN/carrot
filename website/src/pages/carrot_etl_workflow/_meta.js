31720c4bee156f604627f1619f317da7254f1646
Brian Kiiru Kim <kiirubrian21@gmail.com>
2025-12-05 17:18:59 +0000

    ETL documentation (#87)
    * Initial commit
    
    * Completed ETL documentation
    
    * ETL docs improvement
    
    * Added Sample data and improved etl workflow
    
    * Fix PR suggestion
    
    * Added Screenshots to support the white rabbit documentation
    
    * Added Scan report upload image for carrot mapper
    
    * Improve Carrot Transform docs
    
    * Improved Carrot Mapper docs
    
    * Polish Carrot Mapper ETL Docs
    
    * Updated Sample Data Zip File
    
    * Updated format for Data Dict
    
    * Update Carrot Transform requirements
    
    * Move Pre Processing to ETL workflow docs
    
    * Imprive Sample data overview page
    
    * Sample file updates
    
    * Completed updating the ETL Documentation
    
    * Improve Scan report documentaion
    
    * Added more details to mapper docs
    
    * Improve Mapper docs
    
    * Added Steps and improved ETL docs
    
    * Added Steps and improved ETL docs
    
    * Improve Carrot Transform ETL docs
    
    * Improve white rabbit and mapper etl docs
    
    * Improve Transform ETL docs
    
    * Rename PERSON.tsv to person.tsv
    
    * Fix Grammar mistakes
    
    * Fix Grammar mistakes
    
    * Improve mapper docs
    
    * Improve mapper docs
    
    * Improve auto mapping by addind a simple example
    
    * Simplify auto mapping process

---
+++ website/src/pages/carrot_etl_workflow/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  pre_processing: "Pre-Processing",
+  phase1_whiterabbit: "Phase 1: Data Profiling with WhiteRabbit",
+  phase2_mapper: "Phase 2: Data Transformation with Carrot Mapper",
+  phase3_execution: "Phase 3: Data Execution and Validation",
+  resources: "Additional Resources",
+};
